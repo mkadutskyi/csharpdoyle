0a2e5f7b230675efe13576929f117f735d1cb935
Mykola Kadutskyi <mykolakadutskyi@gmail.com>
2017-03-20 17:31:22 -0700

    Initial commit

---
+++ ProgramEx_ch_01_mkadutskyi/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using ProgramEx_ch_01_mkadutskyi.Properties;
+using static System.Console;
+
+//using static ProgramEx_ch_01_mkadutskyi.Properties.CallableFunctions;
+
+namespace ProgramEx_ch_01_mkadutskyi
+{
+    internal class Program
+    {
+        private static readonly string[] FunctionList = {"HelloWorld", "InfoBanner", "XOutput"};
+        protected static string MsgBoxTxt;
+        protected static MessageBox DialogBox;
+
+        public static void Main(string[] args)
+        {
+            int exNum;
+            string userInput;
+
+            CreateMenu();
+            WriteLine(
+                "Hi. This is application containing execercises for the book \"C# Programming Barbara Doyle\"");
+            WriteLine(
+                "************************************************************************************************************");
+
+            while (true)
+            {
+                ShowMenu();
+                userInput = ReadLine();
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Environment.Exit(1);
+                }
+
+                if (Int32.TryParse(userInput, out exNum))
+                {
+                    WriteLine("\n*******************************\nProgram " + FunctionList[exNum - 1] +
+                              " started \n*********************************");
+                    RunFunction(FunctionList[exNum - 1]);
+                    WriteLine("\n*******************************\nProgram " + FunctionList[exNum - 1] +
+                              " stopped its execution \n*********************************");
+                }
+            }
+        }
+
+        static object RunFunction(string functionName)
+        {
+            MethodInfo[] methodInfos =
+                typeof(CallableFunctions).GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            var callableFunctions = new CallableFunctions();
+            return methodInfos.First(mi => mi.Name == functionName).Invoke(callableFunctions, null);
+        }
+
+        static void ShowMenu()
+        {
+            WriteLine("Here is the List of Programmes: \n");
+            WriteLine(MsgBoxTxt);
+            WriteLine(
+                "Type index number of the programm you want to run and press \"Enter\" or just press \"Enter\" to stop application.\n");
+        }
+
+        static void CreateMenu()
+        {
+            for (int i = 0; i < FunctionList.Length; i++)
+            {
+                MsgBoxTxt += i + 1 + ". " + FunctionList[i] + "\n";
+            }
+        }
+    }
+}
---
+++ ProgramEx_ch_01_mkadutskyi/Properties/CallableFunctions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+
+namespace ProgramEx_ch_01_mkadutskyi.Properties
+{
+    public class CallableFunctions
+    {
+        public void HelloWorld()
+        {
+            WriteLine("Hello World! My Name is Mykola Kadutskyi!");
+        }
+
+        public void InfoBanner()
+        {
+            WriteLine("***************************************************************");
+            WriteLine("**     Programmin Assignment #2                              **");
+            WriteLine("**     Developer: Mykola Kadutskyi                           **");
+            WriteLine("**     Date Submitted: 01/01/1970                            **");
+            WriteLine("**     Purpose: Provide Internal Documentation               **");
+            WriteLine("***************************************************************");
+        }
+
+        public void XOutput()
+        {
+            WriteLine("This program will output ");
+            int Width = 100;
+            int Height = 21;
+            int checkSide = 0;
+            if (Width < Height)
+            {
+
+            }
+            for (int i = 0; i < Height; i++)
+            {
+                if (checkSide == 0)
+                {
+                    string before = new String(' ', Width / Height * i);
+                    string after = new String(' ', Width - 2 * i * Width / Height);
+
+                    WriteLine(before + "##" + after + "##");
+                    if (Width / Height * i - Width + 2 * i * Width / Height == Width / 2)
+                    {
+                        checkSide = 1;
+                    }
+                }
+                else
+                {
+                    string before = new String(' ', Width - Width / Height * i);
+                    string after = new String(' ', 2 * i * Width / Height - Width);
+
+                    WriteLine(before + "##" + after + "##");
+                }
+            }
+        }
+    }
+}
